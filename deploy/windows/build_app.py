6e9ac7c2210c9c5873b5bf4f70d4d1db01847e30
Eric Pellegrini <ericpellegrini76@gmail.com>
2020-10-09 10:19:27 +0200

    added deployment files and scripts for windows installer

---
+++ deploy/windows/build_app.py
@@ -0,0 +1,71 @@
+@echo off
+rem the path to the python installer executable
+set python_installer = %1
+
+rem the path to the git directory for lightcycler
+set lightcycler_git_dir = %2
+
+rem the version to install
+set lightcycler_version = %3
+
+rem the directory that will contain the python + deps + lightcycler
+set target_dir = %4
+
+rem remove(if existing) target directory
+rmdir / S / Q % target_dir%
+
+rem create the target directory that will contains the python installation
+mkdir % target_dir%
+
+rem uninstall python
+%python_installer % /quiet / uninstall
+
+rem install python to the select target directory
+%python_installer % /quiet TargetDir = %target_dir%
+
+rem the path to pip executable
+set pip_exe = %target_dir %\Scripts\pip.exe
+
+rem install dependencies
+%pip_exe % install numpy
+%pip_exe % install matplotlib
+%pip_exe % install pandas
+%pip_exe % install PyQt5
+%pip_exe % install openpyxl
+%pip_exe % install tabula-py
+%pip_exe % install scipy
+%pip_exe % install scikit-posthocs
+%pip_exe % install xlrd
+
+rem the path to python executable
+set python_exe = %target_dir %\python.exe
+
+rem cleanup previous installations
+cd % target_dir %\Lib\site-packages
+for / f % %i in ('dir /a:d /S /B lightcycler*') do rmdir / S / Q % %i
+if exist % target_dir %\Scripts\lightcycler(
+    del / Q % target_dir %\Scripts\lightcycler
+)
+
+rem checkout selected version of the lightcycler project
+cd % lightcycler_git_dir%
+%git_exe % fetch - -all
+%git_exe % checkout % lightcycler_version%
+
+rem build and install lightcycler using the python installed in the target directory
+rmdir / S / Q % lightcycler_git_dir %\build
+%python_exe % setup.py build install
+
+rem copy the LICENSE and CHANGELOG files
+copy LICENSE % lightcycler_git_dir %\deploy\windows
+copy CHANGELOG % lightcycler_git_dir %\deploy\windows\CHANGELOG.txt
+
+rem the path to nsis executable
+set makensis = "C:\Program Files (x86)\NSIS\Bin\makensis.exe"
+set nsis_installer = %lightcycler_git_dir %\deploy\windows\lightcycler_installer.nsi
+
+del / Q % target_dir %\lightcycler-%lightcycler_version % -win-amd64.exe
+
+%makensis % /V4 / Onsis_log.txt / DVERSION = %lightcycler_version % /DARCH = win-amd64 / DTARGET_DIR = %target_dir % %nsis_installer%
+
+cd % current_dir%
